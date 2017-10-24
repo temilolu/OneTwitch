51449c7c5bad49fb1746980d3883392de61a8a6e
temilolu <temitayo094@gmail.com>
2017-10-24 16:16:12 +0100

    Set states

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import preload from '../db.json';
 
-const Search = () => (
-	<div>
-		<div>
-			{preload.users.map(users => (
-				<div>
-					<h3>{users.first_name}</h3>
-					<p>{users.email}</p>
-				</div>
-			))}
-		</div>
+class Search extends Component {
+	constructor(props) {
+		super(props);
 
-		<div>
-			{preload.games.map(games => (
+		this.state = {
+			searchTerm: 'this is some sort of debug statement',
+		};
+		this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+	}
+
+	handleSearchTermChange(event) {
+		this.setState({ searchTerm: event.target.value });
+	}
+	render() {
+		return (
+			<div>
+				<header>
+					<h1>OneTwitch!</h1>
+					<a href="/">Home</a>
+
+					<input placeholder={this.state.searchTerm} onChange={this.handleSearchTermChange} type="text" />
+				</header>
 				<div>
-					<h3>{games.game_name}</h3>
-					<img alt={`${games.game_name} show poster`} src={games.game_img} />
+					{preload.games.map(games => (
+						<div>
+							<h3>{games.game_name}</h3>
+							<img alt={`${games.game_name} show poster`} src={games.game_img} />
+						</div>
+					))}
 				</div>
-			))}
-		</div>
-	</div>
-);
+			</div>
+		);
+	}
+}
 
 export default Search;
